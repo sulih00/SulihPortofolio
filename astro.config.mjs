53ef6f67e01da8ed40e0eb2248a989095a80efab
sulih00 <sulih055@gmail.com>
2026-09-22 10:07:45 +0700

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [vue()],
 
   vite: {
